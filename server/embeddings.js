963f348c776c721bcc8c3fd4cf6fdcb0f549615c
deswalcodes <21cs2014@rgipt.ac.in>
2025-06-14 20:38:18 +0530

    added postgresql for storing vector embeddings

--- server/embeddings.js
+++ server/embeddings.js
@@ -1,13 +1,28 @@
 import { OpenAIEmbeddings } from "@langchain/openai";
-import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { config } from 'dotenv';
+config();
 import { Document } from "@langchain/core/documents";
 import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { PGVectorStore } from "@langchain/community/vectorstores/pgvector"
 
 const embeddings = new OpenAIEmbeddings({
     apiKey: process.env.OPENAI_API_KEY
 });
+export const vectorStore = await PGVectorStore.initialize(embeddings,{
+    postgresConnectionOptions : {
+        connectionString : process.env.DB_URL,
+    },
+    tableName : 'transcripts',
+    columns  : {
+        idColumnName : 'id',
+        vectorColumnName : 'vector',
+        contentColumnName : 'content',
+        metadataColumnName : 'metadata'
+    },
+    distanceStrategy : 'cosine'
+});
 
-export const vectorStore = new MemoryVectorStore(embeddings);
+
 export const addYTVideoToVectorStore = async (videoData) => {
     const docs = [
         new Document({
@@ -22,7 +37,7 @@ export const addYTVideoToVectorStore = async (videoData) => {
         chunkOverlap: 200
     });
     const chunks = await splitter.splitDocuments(docs);
-    
+
     await vectorStore.addDocuments(chunks);
 
 } 
